8708457ce1b51da4d4e02b4a4b015b836c9074e1
Derek Rogers <haethos.dr@gmail.com>
2025-09-14 15:37:28 -0700

    updated links in footer. Updated some location pages

--- webpack.config.js
+++ webpack.config.js
@@ -510,10 +510,10 @@ module.exports = {
           {
             test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
             type: 'asset/resource',
-            // generator: {
-            //   // Keep original name and extension
-            //   filename: 'images/[name][ext]'
-            // }
+            generator: {
+              // Keep original name and extension
+              filename: '[name][ext]'
+            }
           },
           {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
