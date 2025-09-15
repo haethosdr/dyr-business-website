6bfb3b43d9a76a25baf11c49b9c7e71d1be4cf65
Derek Rogers <haethos.dr@gmail.com>
2025-09-14 18:27:27 -0700

    updated desktop locations menu and nav location menu

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -92,6 +92,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const serviceMenuLink = document.querySelector('#services');
     const dropDownMenu = document.querySelector('#dropdown-menu');
 
+    const locationsMenuLink = document.querySelector('#locations');
+    const dropDownMenuLocations = document.querySelector('#dropdown-menu-locations');
+
+
+    // services drop down menu
     const toggleMenuDisplay = function () {
       dropDownMenu.style.top = `${serviceMenuLink.offsetTop + 40}px`;
       dropDownMenu.style.left = `${serviceMenuLink.offsetLeft}px`;
@@ -113,6 +118,29 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('dropDownMenu: mouseover');
     });
 
+
+    // locations drop down menu
+    const toggleMenuDisplayLocations = function () {
+      dropDownMenuLocations.style.top = `${locationsMenuLink.offsetTop + 40}px`;
+      dropDownMenuLocations.style.left = `${locationsMenuLink.offsetLeft}px`;
+      dropDownMenuLocations.style.display = 'flex';
+    };
+
+    locationsMenuLink.addEventListener('mouseover', () => {
+        toggleMenuDisplayLocations();
+        console.log('locationsMenuLink: mouseover');
+    });
+
+    locationsMenuLink.addEventListener('mouseout', () => {
+        dropDownMenuLocations.style.display = 'none';
+        console.log("mouseout");
+    });
+
+    dropDownMenuLocations.addEventListener('mouseover', () => {
+        toggleMenuDisplayLocations();
+        console.log('dropDownMenu: mouseover');
+    });
+
     // img slider functionality:
     const quotescrollBtn = [...document.querySelectorAll('.scroll-slider')];
     quotescrollBtn[0].addEventListener('click', scrollLeft);
@@ -123,6 +151,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const navDialog = document.querySelector('#mobile-dialog');
     const closeDiaNav = document.querySelector('.nav-close-btn');
     const servicesSymb = document.querySelector('.dia-menu-symb');
+    const locationsSymb = document.querySelector('.dia-menu-symb-loc');
     
     navOpenBtn.addEventListener('click', () => {
         navDialog.style.display = 'flex';
@@ -134,6 +163,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
         navDialog.style.display = 'none';
         servicesSymb.textContent = '+';
+        locationsSymb.textContent = '+';
     });
     
     // nav subservice menu opening
@@ -146,6 +176,19 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             subNavDisplay.style.display = 'none';
             servicesSymb.textContent = '+';
+        }
+    });
+
+    // nav locatons menu opening
+    const locNavbtn = document.querySelector('#locations-nav');
+    const locNavDisplay = document.querySelector('#loc-srvc-menu');
+        locNavbtn.addEventListener('click', () => {
+        if (locNavDisplay.style.display === 'none') {
+            locNavDisplay.style.display = 'flex';
+            locationsSymb.textContent = '-';
+        } else {
+            locNavDisplay.style.display = 'none';
+            locationsSymb.textContent = '+';
         }
     });
 
